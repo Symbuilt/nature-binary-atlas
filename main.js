c7a6daddf29f0859c08a567cb6864496edc64d27
Symbuilt <rrobinson@symbuilt.com>
2025-06-05 13:24:00 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,1 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import NatureBinaryAtlas from "./NatureBinaryAtlas.js";
 
-const root = createRoot(document.getElementById("root"));
-root.render(<NatureBinaryAtlas />);
